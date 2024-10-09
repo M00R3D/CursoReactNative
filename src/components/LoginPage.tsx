44c8770b8a4fee4bbab4a724e352d63777811a49
job moore <jobmurdan@ohtmail.com>
2024-10-09 16:27:26 -0700

    login y logout sencillo con zustand

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import { useAuthStore } from '../store/auth.store'
+import React, { useEffect } from 'react';
+import { useAuthStore } from '../store/auth.store';
 
 export const LoginPage = () => {
+  const authStatus = useAuthStore((state) => state.status);
+  const user = useAuthStore((state) => state.user);
+  const login = useAuthStore((state) => state.login);
+  const logout = useAuthStore((state) => state.logout);
 
-    const authStatus = useAuthStore(state => state.status);
+  useEffect(() => {
+    setTimeout(() => {
+      logout();
+    }, 1500);
+  }, [logout]);
 
+  if (authStatus === 'checking') {
+    return <h3>Loading ...</h3>;
+  }
 
   return (
     <>
-        <h3>LoginPage</h3>
+      <h3>LoginPage</h3>
 
-        {authStatus}
+      {
+        (authStatus === 'authenticated')
+        ? (
+          // <div>Autenticado como: {JSON.stringify(user, null, 2)}</div>
+          <button onClick={logout}>Logout</button>
+        ) 
+        
+        : (
+          <button onClick={()=>login('John Doe','ABC12')}>
+            Login
+          </button>
+        )
+      }
     </>
-  )
-}
+  );
+};
--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -1,16 +1,32 @@
-import  {create}from 'zustand';
-interface AuthState{
-    status : 'authenticated'|'unaunthenticated'|'checking';
-    token?:string;
-    user?:{
-        name:string;
-        email:string;
+import { create } from 'zustand';
 
-    }
+interface AuthState {
+  status: 'authenticated' | 'unauthenticated' | 'checking';
+  token?: string;
+  user?: {
+    name: string;
+    email: string;
+  };
+  login: (email: string, password: string) => void;  // Actualizamos el tipo de login
+  logout: () => void;
 }
 
-export const useAuthStore=create<AuthState>()(()=>({
-    status :'checking',
-    token:undefined,
-    user:undefined,
+export const useAuthStore = create<AuthState>((set) => ({
+  status: 'checking',
+  token: undefined,
+  user: undefined,
+  login: (email: string, password: string) => {
+    set({
+      status: 'authenticated',
+      token: 'ABC12',
+      user: {
+        name: 'John Doe',
+        email: email,
+      },
+    });
+  },
+
+  logout: () => {
+    set({ status: 'unauthenticated', token: undefined, user: undefined });
+  },
 }));
