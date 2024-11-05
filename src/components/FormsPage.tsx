761c8af48984a9e5d2ff56bebc32ce621ec0f2b9
job moore <jobmurdan@ohtmail.com>
2024-11-04 22:20:48 -0700

    avance del login

--- src/components/FormsPage.tsx
+++ src/components/FormsPage.tsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import {useForm} from 'react-from-hook'
+
+
+type FormInputs = {
+  email:string;
+  password:string;
+}
 
 export const FormsPage = () => {
+
+  const {register} = useForm<FormInputs>({
+    defaultValues:{
+      email:'job@mail.com',
+      password:'666',
+    }
+  });
+
   return (
     <>
         <form action="">
